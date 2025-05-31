8013751a48ff3b7f49ed06df7264a6325e6b091b
GnomoCarek <renanamoriim@gmail.com>
2025-05-31 19:38:16 -0300

    Data e serviço pronto

---
+++ src/sevico/index.js
@@ -0,0 +1,17 @@
+import { produtos} from "../data/data_produtos.js";
+
+export const retornarProdutos = () => {
+  return produtos;
+};
+
+export const buscarProduto = (textoDigitado) => {
+  return produtos.filter(
+    (produto) =>
+    produto.nome.toLowerCase().includes(textoDigitado.toLowerCase()) ||
+    produto.descricao.toLowerCase().includes(textoDigitado.toLowerCase())
+  );
+};
+
+export const filtrarProdutos = (categoria) => {
+  return produtos.filter((produto) => produto.categoria === categoria);
+};
